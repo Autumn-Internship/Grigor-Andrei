97b7eb8452f2385bca866963f22119aa24fa98d6
Grigor Andrei <andrei.grigor@endava.com>
2021-10-20 18:01:21 +0300

    Uploading Counting Valleys Challenge

---
+++ JSChallenges/CountingValleys.js
@@ -0,0 +1,17 @@
+const n=8;
+const p='DUUDDUUD';
+
+let valleys=0;
+let elev=0;
+
+for(let i =0;i<n;i++){
+    if(p[i]=='D'){
+        elev--;
+    }
+    else{
+        if(elev==-1) valleys++;
+        elev++;
+    }
+}
+
+console.log(valleys);
