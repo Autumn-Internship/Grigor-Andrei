eb0db33e2f4d05745749d4ff118928edc18c93ea
Grigor Andrei <andrei.grigor@endava.com>
2021-10-20 17:53:00 +0300

    Uploading RepeatedStrings Challenge

---
+++ JSChallenges/RepeatedStrings.js
@@ -0,0 +1,9 @@
+const s="aba";
+const n=10;
+
+const as = [...s].filter(char => char === "a").length;
+const times = parseInt(n/s.length);
+const remainder = n % s.length;
+
+const totalAs = times * as + [...s.slice(0, remainder)].filter(c => c === "a").length
+console.log(totalAs);
